869d7378bff791288bd75d57862d78115c0ebbb6
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-07-10 20:17:23 -0400

    primeras pruebas con react-router v4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Prueba from './Prueba';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Router>
+        <App/>
+    </Router>    
+, document.getElementById('root'));
+
