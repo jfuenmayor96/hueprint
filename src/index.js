50e2b0dd9ac3deaeb412104749665479955fdf44
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-07-16 16:19:32 -0400

    se eliminan archivos innecesarios de create-react-app

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Prueba from './Prueba';
-import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 
 ReactDOM.render(
     <Router>
         <App/>
-    </Router>    
+    </Router>
 , document.getElementById('root'));
-
