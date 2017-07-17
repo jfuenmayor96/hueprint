911a0518dedcd140f38d483f2d1f30c57aab1cb2
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-07-17 11:14:45 -0400

    se le colocan columnas de bootstrap a la frase del main section

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -9,9 +9,9 @@ class Main extends Component {
                     <div className="row">
                         <div className="main col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <div style={{ paddingTop: "25px" }}>
-                                <img className="img-responsive" src={require("../../assets/Main/bolsa.png")} alt="" />
+                                <img className="img-responsive" height="90%" src={require("../../assets/Main/bolsa.png")} alt="" />
                             </div>
-                            <div style={{backgroundColor: "rgba(255, 255, 255, 0.8)"}}>
+                            <div  className="col-lg-12" style={{backgroundColor: "rgba(255, 255, 255, 0.8)"}}>
                                 <h2 style={{ marginTop: "0px"}}>Impulsa tu marca siendo parte de esta gran comunidad para comercializar, innovar e interactuar</h2>
                             </div>
                         </div>
