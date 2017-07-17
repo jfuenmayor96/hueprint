b4de288e755c77fe6b7c610ba8dfcfbe22b13506
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-07-17 11:14:03 -0400

    primera version de la sección Conócenos

--- src/containers/Conocenos/Conocenos.js
+++ src/containers/Conocenos/Conocenos.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+import "./Conocenos.css";
 
 class Conocenos extends React.Component {
   render () {
@@ -8,14 +9,14 @@ class Conocenos extends React.Component {
             <div className="row">
                 <div className="main col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div style={{ paddingTop: "25px" }}>
-                        <img className="img-responsive" src={require("../../assets/Main/bolsa.png")} alt="" />
+                        <img className="img-responsive" height="90%" src={require("../../assets/Main/bolsa.png")} alt="" />
                     </div>
-                    <div style={{backgroundColor: "rgba(255, 255, 255, 0.8)"}}>
-                        <h2 style={{ marginTop: "0px"}}>Impulsa tu marca siendo parte de esta gran comunidad para comercializar, innovar e interactuar</h2>
+                    <div className="col-lg-12" style={{backgroundColor: "rgba(255, 255, 255, 0.8)"}}>
+                        <h2 style={{ marginTop: "0px", color: "black"}}>COMERCIALIZA <span>●</span> INNOVA <span>●</span> INTERACTUA</h2>
                     </div>
                 </div>
             </div>
-        </div>
+          </div>
       </div>
 
     );
