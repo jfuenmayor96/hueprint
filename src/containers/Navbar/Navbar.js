8badbc1df6b79dcec3fc7721defcc21a5e1b0271
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-07-24 10:34:57 -0400

    se agrega la ruta para la seccion de Huellas

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -21,7 +21,7 @@ class Navbar extends Component {
                         <div className="collapse navbar-collapse navbar-inner text-center navbar-right" id="myNavbar">
                             <ul className="nav navbar-nav text-center" id="elements">
                                 <li><Link to="/hueprint/conocenos/">CONÓCENOS</Link></li>
-                                <li><Link to="/huellas">HUELLAS</Link></li>
+                                <li><Link to="/hueprint/huellas">HUELLAS</Link></li>
                                 <li><Link to="/servicios">SERVICIOS</Link></li>
                                 <li><Link to="/ayuda">AYUDA</Link></li>
                                 <li><Link to="/buscar"><img src={require('../../assets/Navbar/lupa.png')}  height="25px" width="25px" alt="Buscar"/> BUSCAR</Link></li>
