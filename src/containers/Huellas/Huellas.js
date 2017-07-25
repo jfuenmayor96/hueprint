54026f221aec90cfd114bd26dddfb15edcb1257d
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-07-24 21:59:29 -0400

    se arreglan las rutas de las categorias

--- src/containers/Huellas/Huellas.js
+++ src/containers/Huellas/Huellas.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import { Link, Switch, Route } from 'react-router-dom';
+import Categoria from "./Categoria";
 
 import "./Huellas.css";
 
@@ -11,25 +12,25 @@ class Huellas extends React.Component {
             <div className="row">
               <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 categorias">
                 <ul>
-                  <li><Link to="#">Academias</Link></li>
-                  <li><Link to="#">Artes</Link></li>
-                  <li><Link to="#">Belleza y Salud</Link></li>
-                  <li><Link to="#">Carros</Link></li>
-                  <li><Link to="#">Cursos</Link></li>
+                  <li><Link to={`${this.props.match.url}/Academias`}>Academias</Link></li>
+                  <li><Link to={`${this.props.match.url}/Artes`}>Artes</Link></li>
+                  <li><Link to={`${this.props.match.url}/Belleza y Salud`}>Belleza y Salud</Link></li>
+                  <li><Link to={`${this.props.match.url}/Carros`}>Carros</Link></li>
+                  <li><Link to={`${this.props.match.url}/Cursos`}>Cursos</Link></li>
                 </ul>
                 <ul>
-                  <li><Link to="#">Deportes</Link></li>
-                  <li><Link to="#">Empleos</Link></li>
-                  <li><Link to="#">Entretenimiento</Link></li>
-                  <li><Link to="#">Gastronomía</Link></li>
-                  <li><Link to="#">Inmuebles</Link></li>
+                  <li><Link to={`${this.props.match.url}/Deportes`}>Deportes</Link></li>
+                  <li><Link to={`${this.props.match.url}/Empleos`}>Empleos</Link></li>
+                  <li><Link to={`${this.props.match.url}/Entretenimiento`}>Entretenimiento</Link></li>
+                  <li><Link to={`${this.props.match.url}/Gastronomía`}>Gastronomía</Link></li>
+                  <li><Link to={`${this.props.match.url}/Inmuebles`}>Inmuebles</Link></li>
                 </ul>
                 <ul>
-                  <li><Link to="#">Moda</Link></li>
-                  <li><Link to="#">Profesionales</Link></li>
-                  <li><Link to="#">Proyectos</Link></li>
-                  <li><Link to="#">Servicios</Link></li>
-                  <li><Link to="#">Tecnología</Link></li>
+                  <li><Link to={`${this.props.match.url}/Moda`}>Moda</Link></li>
+                  <li><Link to={`${this.props.match.url}/Profesionales`}>Profesionales</Link></li>
+                  <li><Link to={`${this.props.match.url}/Proyectos`}>Proyectos</Link></li>
+                  <li><Link to={`${this.props.match.url}/Servicios`}>Servicios</Link></li>
+                  <li><Link to={`${this.props.match.url}/Tecnología`}>Tecnología</Link></li>
                 </ul>
               </div>
             </div>
@@ -37,10 +38,10 @@ class Huellas extends React.Component {
             <div className="row publicaciones" style={{paddingTop: "30px", paddingBottom: "30px"}}>
               <div className="col-lg-10 contenedor">
                 <Switch>
-                  <Route path="/huellas/academias" />
+                  <Route path={`${this.props.match.url}/:categoria`} component={Categoria}/>
                 </Switch>
               </div>
-              <div className="col-lg-1"></div>
+              <div className="col-lg-2" style={{backgroundColor: "white", height: "417px"}}></div>
             </div>
           </div>
 
