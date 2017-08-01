6a9f197b49ce26f66e80a5bda9f044082687655f
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-08-01 17:33:04 -0400

    se obtiene la información de la API y se renderiza

--- src/containers/Huellas/Categoria.js
+++ src/containers/Huellas/Categoria.js
@@ -5,16 +5,67 @@ import "./Categoria.css";
 class Categoria extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props);
+    this.fetchInfo = this.fetchInfo.bind(this);
+    //this.state = {query: []}
+    this.query = [];
   }
+
+  fetchInfo(categoria) {
+   if(categoria) {
+     fetch('https://hueprint.herokuapp.com/api/huellas/' + categoria)
+       .then(res => res.json())
+         .then(query => {
+           this.query = query;
+           this.forceUpdate();
+         })
+       .catch(err => console.log(err.stack) );
+   }
+   else {
+     fetch('https://hueprint.herokuapp.com/api/mostrarHuellas')
+       .then(res => res.json())
+         .then(query => {
+           this.query = query;
+           console.log(query);
+           this.forceUpdate();
+         })
+       .catch(err => console.log(err.stack) );
+   }
+
+  }
+
+
+  componentWillReceiveProps(nextProps) {
+    //console.log("next props: ");
+    //console.log(nextProps.match.params.categoria);
+    //nextProps.match.params.categoria
+    this.fetchInfo(nextProps.match.params.categoria);
+    this.forceUpdate();
+    //this.setState(this.state);
+  }
+
+
   render () {
     return(
       <div style={{backgroundColor: "white"}}>
         <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <h1>{this.props.match.params.categoria}</h1>
-        </div>
+          <h1>{this.props.match.params.categoria ? this.props.match.params.categoria : "Selecciona una categoría" }</h1>
 
-        <div className="col-lg-4  col-md-4 col-sm-6 col-xs-12">
+        </div>
+        {this.query.map(huella => {
+          return(
+            <div className="col-lg-4  col-md-4 col-sm-6 col-xs-12" key={huella.nombre}>
+              <Link to="#" className="thumbnail">
+                <img className="publicacion" src={huella.logo} alt="Imagen publicacion" height="300px" width="300px"/>
+                <div style={{textAlign: "center"}}>
+                  <p>{huella.nombre}</p>
+                  <p>{huella.estado}</p>
+                  <p>{huella.clasificacion}</p>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+        {/*}<div className="col-lg-4  col-md-4 col-sm-6 col-xs-12">
           <Link to="#" className="thumbnail">
             <img className="publicacion" src={require("../../assets/Huellas/jf.png")} alt="Imagen publicacion" height="300px" width="300px"/>
             <div style={{textAlign: "center"}}>
@@ -43,7 +94,7 @@ class Categoria extends React.Component {
               <p>Área: diseño web</p>
             </div>
           </Link>
-        </div>
+        </div>*/}
       </div>
 
     );
