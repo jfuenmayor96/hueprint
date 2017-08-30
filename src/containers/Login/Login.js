f650c091971734993371173ef4385de0f256f8ff
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-08-30 18:53:20 -0400

    se agrega la opcion de recuperar contrasena. Si hay una sesion activa, al ir a Login, se redirige a Usuario

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -6,6 +6,7 @@ class Login extends React.Component {
   constructor(props){
    super(props);
    this.handleSubmit = this.handleSubmit.bind(this);
+   this.recuperarClave = this.recuperarClave.bind(this);
   }
 
   handleSubmit(event){
@@ -33,6 +34,63 @@ class Login extends React.Component {
      .catch(err => console.log(err));
   };
 
+  recuperarClave(e) {
+    e.preventDefault();
+    if(document.getElementById("email").value === "" || document.getElementById("email").value === undefined){
+      alert("Introduzca un correo electrónico válido en el campo de Correo electrónico");
+      document.getElementById("labelCorreo").style.color =  "#FFF800";
+    }
+    else{
+      fetch('https://hueprint.herokuapp.com/api/recuperarClave', {
+      method: 'post',
+      credentials: 'include',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+        "email": document.getElementById("email").value
+      })
+      })
+        .then(response => response.json())
+        .then(res => {
+          switch (res) {
+            case "ok":
+              alert("Contraseña enviada al correo electrónico " + document.getElementById("email").value);
+              break;
+            case "usuario no encontrado":
+              alert("Usuario no registrado en el sistema.");
+              break;
+            default:
+              alert("Ha ocurrido un error. Póngase en contacto con un administrador.");
+              break;
+          }
+        })
+        .catch(err => console.log(err));
+     };
+    }
+
+    componentDidMount(){
+      /*Obtiene la información de la sesión del usuario*/
+      fetch("https://hueprint.herokuapp.com/api/sessions", {
+      method: 'GET',
+      credentials: 'include',
+      redirect: 'follow',
+      agent: null,
+      headers: {
+          'Content-Type':'application/json'
+      },
+      timeout: 5000
+    })
+      .then(response => response.json())
+      .then(res => {
+        this.setState({"email": res.email, "huella": res.huella});
+
+        if(res.isLoggedIn != undefined){
+          this.props.history.push("/hueprint/usuario");
+
+        }
+      })
+      .catch(err => console.log(err));
+    }
+
   render () {
     return(
       <div>
@@ -41,7 +99,7 @@ class Login extends React.Component {
             <h2>Iniciar sesión</h2><br/>
               <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
-                  <label htmlFor="email">Correo electrónico:</label>
+                  <label id="labelCorreo" htmlFor="email">Correo electrónico:</label>
                   <input type="email" className="form-control" id="email" name="email"/>
                 </div>
                 <div className="form-group">
@@ -50,6 +108,7 @@ class Login extends React.Component {
                 </div>
                 <center><button type="submit" className="btn btn-default">Submit</button></center>
               </form>
+              <center><span onClick={this.recuperarClave} style={{display: "block", color: "white", paddingTop: "10px", cursor: "pointer"}}>Olvidé mi contraseña</span></center>
           </div>
         </div>
       </div>
