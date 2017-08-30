061dc3e4de15e21f9e29602a6fb5dd3fa253c538
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-08-30 18:54:21 -0400

    se agregan los links para cerrar sesion y administrar cuenta. Se muestran las opciones segun si se ha iniciado sesion, o no

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import "./Navbar.css";
 
 class Navbar extends Component {
+    constructor(props) {
+      super(props);
+      this.cerrarSesion = this.cerrarSesion.bind(this);
+    }
+
+    cerrarSesion(e) {
+      e.preventDefault();
+      alert("entre!");
+      fetch("https://hueprint.herokuapp.com/api/logout", {
+      method: 'GET',
+      credentials: 'include',
+      redirect: 'follow',
+      agent: null,
+      headers: {
+          'Content-Type':'application/json'
+      },
+      timeout: 5000
+    })
+      .then(response => response.json())
+      .then(res => {
+        console.log(res);
+        if(res === "ok"){
+          document.getElementById("usuario").style.display = "none";
+          document.getElementById("logout").style.display = "none";
+          document.getElementById("login").style.display = "inline-block";
+          document.getElementById("registro").style.display = "inline-block";
+          this.props.history.push("/hueprint/");
+        }
+      })
+      .catch(err => console.log(err));
+    };
+
+
     render() {
         return (
             <div>
@@ -23,8 +56,10 @@ class Navbar extends Component {
                                 <li><Link to="/hueprint/conocenos/">CONÓCENOS</Link></li>
                                 <li><Link to="/hueprint/huellas">HUELLAS</Link></li>
                                 <li><Link to="/hueprint/servicios">PLANES Y TARIFAS</Link></li>
-                                <li><Link to="/hueprint/login">INICIAR SESIÓN</Link></li>
-                                <li><Link to="/hueprint/registro/">REGISTRARSE</Link></li>
+                                <li id="login" style={{display: "inline-block"}}><Link to="/hueprint/login">INICIAR SESIÓN</Link></li>
+                                <li id="registro" style={{display: "inline-block"}}><Link to="/hueprint/registro/">REGISTRARSE</Link></li>
+                                <li id="usuario" style={{display: "none"}}><Link to="/hueprint/usuario">ADMINISTRAR CUENTA</Link></li>
+                                <li id="logout" style={{display: "none"}} onClick={this.cerrarSesion}><Link to="/hueprint/">CERRAR SESIÓN</Link></li>
                                 <li><Link to="/buscar"><img src={require('../../assets/Navbar/lupa.png')}  height="25px" width="25px" alt="Buscar"/> BUSCAR</Link></li>
                                 <li><Link to="/traducir"><img src={require('../../assets/Navbar/planeta.png')}  height="25px" width="25px" alt="Traducir"/> TRADUCIR</Link></li>
                             </ul>
@@ -37,5 +72,5 @@ class Navbar extends Component {
     }
 }
 
-
+const ShowTheLocationWithRouter = withRouter(Navbar);
 export default Navbar;
